600697682bcb816a864a4bc91b0a09846adf3c3f
Dima Sukhikh <coddeys@gmail.com>
2016-10-25 13:54:35 +0200

    projectile

--- custom/projectile-config.el
+++ custom/projectile-config.el
@@ -4,11 +4,18 @@
 (use-package projectile
   :ensure t)
 
-;; (projectile-global-mode)
-;; (add-hook 'projectile-mode-hook 'projectile-rails-on)
+(use-package projectile-rails
+  :ensure t)
+
+(projectile-global-mode)
+(projectile-rails-global-mode)
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
+
+(use-package helm-ag
+  :ensure t)
 
 (use-package helm-projectile
   :ensure t)
 
-;; (helm-projectile-on)
+(helm-projectile-on)
 ;;; projectile-config.el ends here
--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 ;;; package --- Emacs init.el
 ;;; Commentary:
+;;; Code:
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")
@@ -37,4 +38,18 @@
 (load "helm-cnfg.el")
 (load "google-translate-config.el")
 ;; (load "wanderlust-config.el")
-;; ;;; init.el ends here
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (helm-ag zenburn-theme yaml-mode web-mode w3m use-package smartparens ruby-refactor ruby-block rubocop robe projectile-rails multi-term magit json-mode js2-mode highlight-indentation helm-projectile google-translate flycheck fill-column-indicator discover company-web ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
