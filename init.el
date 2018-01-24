3227991d5ff3dc57684fde0d8bc2759569536bb1
Coddeys <coddeys@gmail.com>
2018-01-24 14:59:09 +0100

    Use js2-mode for js files

--- init.el
+++ init.el
@@ -152,21 +152,20 @@ scroll-preserve-screen-position 1)
   (setq web-mode-script-padding 2)
   (setq web-mode-style-padding 2))
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 
 ;; for better jsx syntax-highlighting in web-mode
 ;; - courtesy of Patrick @halbtuerke
-(defadvice web-mode-highlight-part (around tweak-jsx activate)
-  (if (equal web-mode-content-type "jsx")
-    (let ((web-mode-enable-part-face nil))
-      ad-do-it)
-    ad-do-it))
+;; (defadvice web-mode-highlight-part (around tweak-jsx activate)
+;;   (if (equal web-mode-content-type "jsx")
+;;     (let ((web-mode-enable-part-face nil))
+;;       ad-do-it)
+;;     ad-do-it))
 
 (use-package js2-mode
   :ensure t
   :config
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
   (add-hook 'js2-mode-hook
             (lambda ()
               (setq js2-basic-offset 2))))
@@ -193,7 +192,7 @@ scroll-preserve-screen-position 1)
   (setq-default flycheck-disabled-checkers
                 (append flycheck-disabled-checkers
                         '(json-jsonlist)))
-  (flycheck-add-mode 'javascript-eslint 'web-mode))
+  (flycheck-add-mode 'javascript-eslint 'js2-mode))
 
 (flycheck-define-checker ruby-rubocop
   "A Ruby syntax and style checker using the RuboCop tool."
